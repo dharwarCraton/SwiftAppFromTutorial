5b26206b0f55f443b6ebbac0dc705ee50f2279d8
dharwarCraton <fnalam@bsugmail.net>
2017-05-01 11:17:16 -0400

    Boolean fun.

--- SwiftTutorialApp/ViewController.swift
+++ SwiftTutorialApp/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var topLabel: UILabel!
     
     @IBOutlet weak var text1: UITextField!
@@ -22,15 +22,21 @@ class ViewController: UIViewController {
     
     @IBAction func buttonTapped(_ sender: Any) {
         
-        topLabel.text = " Answer: \((Double(text1.text!)! + Double(text2.text!)!))"
+        let addition = false
+        
+        if addition {
+            topLabel.text = " Answer: \((Double(text1.text!)! + Double(text2.text!)!))"
+        } else {
+            topLabel.text = " Answer: \((Double(text1.text!)! - Double(text2.text!)!))"
+        }
         
         /*tapCount = tapCount + 1
-        
-        // to count number of times the button was tapped. Create this inside the connect action func.
-        if tapCount >= 10 {
-            
-            topLabel.text = "You tapped PUSH ME 10 or more times"
-        }*/
+         
+         // to count number of times the button was tapped. Create this inside the connect action func.
+         if tapCount >= 10 {
+         
+         topLabel.text = "You tapped PUSH ME 10 or more times"
+         }*/
     }
     
     @IBAction func buttonTapped2(_ sender: Any) {
@@ -39,18 +45,18 @@ class ViewController: UIViewController {
         
         
         /*topLabel.text = "This text changed on PUSH ME TOO"
-        print("Button tapped again")*/
+         print("Button tapped again")*/
     }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
